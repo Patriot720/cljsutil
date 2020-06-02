ee1e672578e042559324d2c9561d7958a7fd2dd6
patriot720 <cerkin-3@yandex.ru>
2020-06-02 21:26:05 +0300

    modified:   project.clj

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "Util for cljs projects"
   :license {:name "GPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :scm {:name "git" :url "https://github.com/Patriot720/cljsutil"}
   :dependencies []
   :source-paths ["src"])
